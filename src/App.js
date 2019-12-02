95f3e5d0714a1bd4483784128006e160930f98f3
Charles Gourdin <gourdin.charles@gmail.com>
2019-12-02 11:31:40 +0100

    Ok pour début Quête

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import LightContainer from './components/LightContainer';
 
 function App() {
   return (
     <div className="App">
-      <LightContainer/>
     </div>
   );
 }
